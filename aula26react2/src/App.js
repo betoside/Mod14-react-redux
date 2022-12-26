f5ba562df74e0fdaf4b8529b3c90fe63add71b91
Betoside <betoside@gmail.com>
2022-12-26 16:07:36 -0300

    33: React Router (parte 3), INCOMPLETE, com erro no BrowserRouter

--- aula26react2/src/App.js
+++ aula26react2/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
@@ -6,10 +7,13 @@ import { Home } from './pages/Home';
 class App extends Component{
     render(){
         return(
-            <div>
-                <Header />
-                <Home />
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    {/* <Home /> */}
+                    <Route exact path="/b7-Frontend-Zero-Ao-Pro/Mod14-react-redux/aula26react2/public/" component={Home} />
+                </div>
+            </BrowserRouter>
         );
     }
 }
